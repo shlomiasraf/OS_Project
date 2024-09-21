4109d0bd7a131bc1a47243549fffdaa565a8cd4d
ShoamIv <155763643+ShoamIv@users.noreply.github.com>
2024-09-21 16:34:08 +0300

    Add files via upload

---
+++ LF_Final/ThreadPool.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include <vector>
+#include <thread>
+#include <functional>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+
+class ThreadPool {
+public:
+    ThreadPool(size_t numThreads);
+    ~ThreadPool();
+
+    // Enqueue a new task
+    void enqueue(std::function<void()> task);
+
+    // Start the thread pool
+    void start();
+
+    // Stop the thread pool
+    void stop();
+
+private:
+    std::vector<std::thread> workers; // Worker threads
+    std::queue<std::function<void()>> tasks; // Task queue
+    std::mutex tasksMutex; // Mutex for protecting task queue
+    std::condition_variable condition; // Condition variable to notify workers
+    bool stopFlag; // Flag to indicate stopping
+    size_t activeLeaderIndex; // Track the current leader thread
+};
---
+++ LF_Final/main.cpp
@@ -0,0 +1,86 @@
+
+#include "ThreadPool.hpp"
+#include "RequestHandling.hpp"
+
+Graph graph(0,0);
+
+void handleClient(int client_fd, ThreadPool& pool) {
+    RequestHandling requestHandler(graph);
+    requestHandler.processClient(client_fd,pool);
+}
+
+int setup_server() {
+    int server_fd;
+    struct sockaddr_in address;
+
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(9034);
+
+    int opt = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
+        perror("setsockopt");
+        return -1;
+    }
+    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 3) < 0) {
+        perror("listen");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Server listening on port %d\n", 9034);
+    return server_fd;
+}
+
+int setup_client_connection(int server_fd) {
+    struct sockaddr_storage remoteaddr;
+    socklen_t addrlen = sizeof(remoteaddr);
+    char remoteIP[INET6_ADDRSTRLEN];
+    int new_fd = accept(server_fd, (struct sockaddr*)&remoteaddr, &addrlen);
+
+    if (new_fd == -1) {
+        perror("accept");
+        return -1;
+    }
+
+    if (remoteaddr.ss_family == AF_INET) {
+        struct sockaddr_in* s = (struct sockaddr_in*)&remoteaddr;
+        inet_ntop(AF_INET, &s->sin_addr, remoteIP, sizeof(remoteIP));
+    } else {
+        struct sockaddr_in6* s = (struct sockaddr_in6*)&remoteaddr;
+        inet_ntop(AF_INET6, &s->sin6_addr, remoteIP, sizeof(remoteIP));
+    }
+    printf("New connection from %s on socket %d\n", remoteIP, new_fd);
+
+    return new_fd;
+}
+
+int main() {
+    int server_fd = setup_server();
+    ThreadPool pool(4); // Create a ThreadPool with 4 threads
+    pool.start();
+
+    while (true) {
+        int client_fd = setup_client_connection(server_fd);
+        if (client_fd != -1) {
+            // Enqueue client command handling
+                handleClient(client_fd,pool); // Handle client requests and enqueue commands
+           
+        }
+    }
+
+    close(server_fd);
+    pool.stop();
+    return 0;
+}
